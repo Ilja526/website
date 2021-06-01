81a01fdcd78d2c851c59df41ba309ebb775885a2
Anatolijs Zvingulis <78356467+04DPAZvingulis@users.noreply.github.com>
2021-06-01 15:54:37 +0300

    поправка питона

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 <p>Viesnica</p>
 <p>DP1-4</p>
 <p>Anatolijs Zvingulis, Ilja Ivanenkovs, Nikita Sergejevs, Davids Sļotovs</p>
-<p>https://www.figma.com/file/cCYfIvOmXosCC2ZWucGeh3/Untitled?node-id=0%3A1/p<>
+<p>https://www.figma.com/file/cCYfIvOmXosCC2ZWucGeh3/Untitled?node-id=0%3A1</p>
--- main.py
+++ main.py
@@ -7,9 +7,16 @@ def main():
   return render_template("main.html")
 
 
-@app.route('/about')
-def about():
-  return render_template("about.html")
+@app.route('/login')
+def login():
+  return render_template("login.html")
 
+@app.route('/sign')
+def signup():
+  return render_template("signup.html")
+
+@app.route('/register')
+def register():
+  return render_template("register.html")
 
 app.run(host='0.0.0.0', port=8080)
